737ed6a1b0bc8f88596c2e7becaea4e754dd5d74
vfarcy <internet@farcy.me>
2026-04-06 18:21:49 +0200

    Change nload to htop in tmux session setup

--- monitor_ai.sh
+++ monitor_ai.sh
@@ -7,7 +7,7 @@ tmux has-session -t $SESSION 2>/dev/null
 if [ $? != 0 ]; then
   # Créer la session et lancer nvtop dans le premier panneau
   tmux new-session -d -s $SESSION 'nvtop'
-  # Diviser l'écran horizontalement et lancer nload dans le deuxième panneau
+  # Diviser l'écran horizontalement et lancer htop dans le deuxième panneau
   tmux split-window -h -t $SESSION 'htop'
 fi
 
